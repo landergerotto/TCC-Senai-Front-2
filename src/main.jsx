b2338c478cc672e96944f9b08f7533afc7460701
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-07-01 10:08:04 -0300

    poglog

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import NavbarComponent from './components/Navbar/navbar.jsx';
 
-// import { Analytics } from "@vercel/analytics/react"
-// import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    {/* <SpeedInsights />
-    <Analytics /> */}
+    <SpeedInsights />
+    <Analytics />
     <BrowserRouter>
       <NavbarComponent />
       <App />

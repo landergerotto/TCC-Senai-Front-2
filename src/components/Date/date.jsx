785c6f595d800615f9ca7c2bd7e4a21fcd38797f
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-07-01 10:35:39 -0300

    data

--- src/components/Date/date.jsx
+++ src/components/Date/date.jsx
@@ -1,15 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styles from './date.module.css';
 
-function Date() {
+function DateComponent() {
+    const [currentDate, setCurrentDate] = useState('');
+
+    useEffect(() => {
+        axios.get('http://worldtimeapi.org/api/ip')
+            .then(response => {
+                const datetime = response.data.datetime;
+                const date = new Date(datetime);
+                const day = date.getDate().toString().padStart(2, '0'); // Ensure two digits with leading zero
+                const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Ensure two digits with leading zero
+                const year = date.getFullYear();
+                const formattedDate = `${day}/${month}/${year}`;
+                setCurrentDate(formattedDate);
+            })
+            .catch(error => {
+                console.error('Error fetching date:', error);
+            });
+    }, []);
+
     return (
         <div className={styles.cardBg}>
             <div className={styles.data}>
-                27/06/2024
+                {currentDate}
             </div>
         </div>
-    )
+    );
 }
 
-// "http://worldtimeapi.org/api/ip"
-
-export default Date;
+export default DateComponent;

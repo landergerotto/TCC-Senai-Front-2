3101d14192e3c6570a9013e662703e5b75126ec1
Lander Furusho Gerotto <lander.gertto@gmail.com>
2024-07-02 10:43:28 -0300

    kekw

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/Home'
+import TestPage from './pages/Test'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
+      <Route path='/t' element={<TestPage />}></Route>
     </Routes>
   )
 }
---
+++ src/pages/Test/index.jsx
@@ -0,0 +1,16 @@
+import Date from "../../components/Date/date";
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import styles from './index.module.css';
+
+function TestPage() {
+    return (
+        <>
+            pog
+        </>
+    )
+}
+
+export default TestPage;

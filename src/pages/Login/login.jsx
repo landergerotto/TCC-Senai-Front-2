370b6e9cdd8baa83357176ddda1ecda3425cb530
eric-coutinho <eric.coutinhop@hotmail.com>
2024-07-04 09:47:04 -0300

    Working on login page.

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -3,9 +3,14 @@ import Formulario from "../../components/Formulario/formulario";
 import Input from "../../components/Input/input";
 
 function LoginPage() {
+    const fields = [
+        { label: "Email", type: "email", name: "email" },
+        { label: "Senha", type: "password", name: "password" }
+    ];
+    let actions = ["Entrar", "Cancelar"];
     return (
         <Container>
-            <Formulario title={"Login"} action={"Entrar"}/>
+            <Formulario title={"Login"} fields={fields} actions={actions} />
         </Container>
     )
 }

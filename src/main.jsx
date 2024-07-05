8ba5daa24a9896ad846cbc87eb6dcab5b7b1d116
eric-coutinho <eric.coutinhop@hotmail.com>
2024-07-05 10:35:22 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main.jsx
+++ src/main.jsx
@@ -7,32 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-// import HomePage from './pages/Home/Home.jsx';
-// import TestPage from './pages/Test/Test.jsx';
-// import LoginPage from './pages/Login/Login.jsx';
-import SideNavBar from './components/Sidebar/Sidebar.jsx';
-import NavbarComponent from './components/Navbar/Navbar.jsx';
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element:   <HomePage />,
-//   },
-//   {
-//     path: "/t",
-//     element:   <TestPage />,
-//   },
-//   {
-//     path: "/login",
-//     element:   <LoginPage />,
-//   },
-// ]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <SpeedInsights />

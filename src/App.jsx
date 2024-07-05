db6398fdab3b6065fa6989260c21cc011be1733f
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-07-05 09:45:49 -0300

    fuck

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home'
-import TestPage from './pages/Test'
-import SideNavBar from './components/Sidebar/Sidebar'
-import NavbarComponent from './components/Navbar/navbar.jsx';
-import LoginPage from './pages/Login/login'
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='/t' element={<TestPage />}></Route>
-        <Route path='/login' element={<LoginPage />}></Route>
-      </Routes>
-    </>
-  )
-}
+// import { Routes, Route } from 'react-router-dom'
+// import HomePage from './pages/Home'
+// import TestPage from './pages/Test'
+// import SideNavBar from './components/Sidebar/Sidebar'
+// import NavbarComponent from './components/Navbar/navbar.jsx';
+// import LoginPage from './pages/Login/login'
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path='/' element={<HomePage />}></Route>
+//         <Route path='/t' element={<TestPage />}></Route>
+//         <Route path='/login' element={<LoginPage />}></Route>
+//       </Routes>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,35 @@ import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from './pages/Home/index.jsx';
+import TestPage from './pages/Test/index.jsx';
+import LoginPage from './pages/Login/login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:   <HomePage />,
+  },
+  {
+    path: "/t",
+    element:   <TestPage />,
+  },
+  {
+    path: "/login",
+    element:   <LoginPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <SpeedInsights />
     <Analytics />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+  
   </>,
 )

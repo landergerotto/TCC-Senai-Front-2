04d67e6c7e688732a8709cf2f98a53040fd545c5
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-07-05 09:55:47 -0300

    despair

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import Date from "./components/Date/date";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import styles from './index.module.css';
+import styles from './Home.module.css';
 import Button from "./components/Button/button";
 import { useNavigate } from "react-router-dom";
 

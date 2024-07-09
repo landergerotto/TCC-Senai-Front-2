c927c72bbcd17b0c65fdb73eeee312ed4fc51ada
Eric-Coutinho <Eric.Coutinhop@hotmail.com>
2024-07-09 10:35:44 -0300

    Working on home page, changes to formulario component

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -3,7 +3,7 @@ import styles from './input.module.css';
 function Input({label, type, name, id, onChange}) {
     return (
         <div className={styles.bg}>
-            <label for={name}>{label}</label>
+            <label htmlFor={name}>{label}</label>
             <input type={type} name={name} id={id} onChange={onChange}/>
         </div>
     )

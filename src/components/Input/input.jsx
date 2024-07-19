7a34ed980a42b89b7b4d2e90a6b6ed3661268101
eric-coutinho <eric.coutinhop@hotmail.com>
2024-07-19 09:10:23 -0300

    Homeform not reseting after reloading the page

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './input.module.css';
 
-function Input({ label, type, name, id, onChange, style, labelStyle, select = false, options = [] }) {
+function Input({ label, type, name, id, values, onChange, style, labelStyle, select = false, options = [] }) {
+    const [value, setValue] = useState(localStorage.getItem(name) || '');
+
+    useEffect(() => {
+        localStorage.setItem(name, value);
+    }, [name, value]);
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        onChange(e);
+    };
+
     return (
         <div className={styles.bg}>
             <label htmlFor={name} style={labelStyle}>{label}</label>
             {select ? (
-                <select name={name} id={id} onChange={onChange} style={style}>
+                <select name={name} id={id} onChange={handleChange} style={style} value={value} >
+                    <option value="" disabled>Selecione</option>
                     {options.map((option, index) => (
                         <option key={index} value={option.value} style={style}>
                             {option}
@@ -13,7 +26,7 @@ function Input({ label, type, name, id, onChange, style, labelStyle, select = fa
                     ))}
                 </select>
             ) : (
-                <input type={type} name={name} id={id} onChange={onChange} style={style} />
+                <input type={type} name={name} id={id} onChange={handleChange} style={style} value={value} />
             )}
         </div>
     );

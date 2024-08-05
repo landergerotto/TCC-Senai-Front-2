cbc81f3aa8e97a11873e9b7451454f881b0b4b29
Lander Furusho Gerotto[ <lander.gerotto@gmail.com>
2024-08-05 11:06:23 -0300

    ;--;

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,10 +1,43 @@
-import SideNavBar from "../../components/Sidebar/Sidebar";
-
+import { PowerBIEmbed } from 'powerbi-client-react';
+import { models } from 'powerbi-client';
 
 function TestPage() {
     return (
         <>
-            {/* <SideNavBar></SideNavBar> */}
+            <PowerBIEmbed
+            embedConfig = {{
+            type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+            id: '<Report Id>',
+            embedUrl: '<Embed Url>',
+            accessToken: '<Access Token>',
+            tokenType: models.TokenType.Embed, // Use models.TokenType.Aad for SaaS embed
+            settings: {
+                panes: {
+                    filters: {
+                        expanded: false,
+                        visible: false
+                    }
+                },
+                background: models.BackgroundType.Transparent,
+            }
+            }}
+
+            eventHandlers = {
+                new Map([
+                    ['loaded', function () {console.log('Report loaded');}],
+                    ['rendered', function () {console.log('Report rendered');}],
+                    ['error', function (event) {console.log(event.detail);}],
+                    ['visualClicked', () => console.log('visual clicked')],
+                    ['pageChanged', (event) => console.log(event)],
+                ])
+            }
+
+            cssClassName = { "reportClass" }
+
+            getEmbeddedComponent = { (embeddedReport) => {
+                window.report = embeddedReport;
+            }}
+            />
         </>
     )
 }

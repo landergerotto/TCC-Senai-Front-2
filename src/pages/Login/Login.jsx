732c8d7d90ac724bc9ee8105a5ab76761bcd8695
Eric-Coutinho <eric.coutinhop@hotmail.com>
2024-08-05 10:12:33 -0300

    Preparing for redefine password

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import styles from './Login.module.css'
+import styles from "./Login.module.css";
 
 import { Container, Col } from "react-bootstrap";
 
@@ -7,25 +7,49 @@ import Formulario from "../../components/Formulario/formulario";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const fields = [
-        { label: "Email", type: "email", name: "email" },
-        { label: "Senha", type: "password", name: "password" }
-    ];
-    const actions = [
-        { label: "Entrar", type: "normal" },
-        { label: "Registrar-se", type: "cancel" },
-        { label: "Cancelar", type: "cancel" }
-    ];
+  var recuperaSenha = () => navigate("/recupera");
 
-    return (
-        <Container className={styles.container}>
-            <Col lg={6}>
-                <Formulario title={"Login"} fields={fields} actions={actions} target={'/'} navigate={navigate} labelStyle={{ marginTop: '0.5em' }} url={"auth/login"}/>
-            </Col>
-        </Container>
-    )
+  var underTextStyle = {
+    marginLeft: "0.25em",
+    marginTop: "0.15em",
+    color: "#007BC0",
+    cursor: "pointer"
+  };
+
+  const fields = [
+    { label: "Email", type: "email", name: "email" },
+    {
+      label: "Senha",
+      type: "password",
+      name: "password",
+      underText: "Esqueceu sua senha?",
+      underTextAction: recuperaSenha,
+      underTextStyle: underTextStyle,
+    },
+  ];
+  const actions = [
+    { label: "Entrar", type: "normal" },
+    { label: "Registrar-se", type: "cancel" },
+    { label: "Cancelar", type: "cancel" },
+  ];
+
+  return (
+    <Container className={styles.container}>
+      <Col lg={6}>
+        <Formulario
+          title={"Login"}
+          fields={fields}
+          actions={actions}
+          target={"/"}
+          navigate={navigate}
+          labelStyle={{ marginTop: "0.5em" }}
+          url={"auth/login"}
+        />
+      </Col>
+    </Container>
+  );
 }
 
 export default LoginPage;

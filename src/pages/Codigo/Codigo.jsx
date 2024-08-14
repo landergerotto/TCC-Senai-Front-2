d80815d9e8ca478686e5f57f9f7cc9d23fae78cf
eric-coutinho <eric.coutinhop@hotmail.com>
2024-08-14 10:47:46 -0300

    Finishing code confirmation page

--- src/pages/Codigo/Codigo.jsx
+++ src/pages/Codigo/Codigo.jsx
@@ -1,82 +1,102 @@
 import {
-    Card,
-    CardBody,
-    CardText,
-    CardTitle,
-    Col,
-    Container,
-    Row,
-  } from "react-bootstrap";
-  
-  import styles from "./Codigo.module.css";
-  import Button from "../../components/Button/button";
-  import { useNavigate } from "react-router-dom";
-  import { useRef } from "react";
-  
-  function CodigoPage() {
-    const navigate = useNavigate();
-    const inputRefs = useRef([]);
-  
-    function handleChange(event, id) {
-      const value = event.target.value;
-  
-      if (value && id < inputRefs.current.length - 1) {
-        inputRefs.current[id + 1].focus();
-      }
-    }
-  
-    function handleKeyDown(event, id) {
-      if (event.key === "Backspace") {
-        if (inputRefs.current[id].value === "") {
-          if (id > 0) {
-            inputRefs.current[id - 1].focus();
-          }
-        } else {
-          inputRefs.current[id].value = "";
-        }
-      }
-    }
-  
-    return (
-      <>
-        <Container style={{ marginTop: "8em" }}>
-          <Row className={styles.container}>
-            <Col sm={8} lg={6}>
-              <Card>
-                <CardBody>
-                  <CardTitle className={styles.title}>
-                    Inserir Código de Verificação
-                  </CardTitle>
-                  <CardText className={styles.inputs}>
-                    {Array.from({ length: 6 }).map((_, index) => (
-                      <Col key={index} className={styles.col} xs={1}>
-                        <input
-                          type="number"
-                          id={index}
-                          ref={(el) => (inputRefs.current[index] = el)}
-                          onChange={(e) => handleChange(e, index)}
-                          onKeyDown={(e) => handleKeyDown(e, index)}
-                          maxLength={1}
-                        />
-                      </Col>
-                    ))}
-                  </CardText>
-                  <div className={styles.btnGroup}>
-                    <Button text={"Verificar Código"} />
-                    <Button
-                      text={"Cancelar"}
-                      type={"cancel"}
-                      onClick={() => navigate("/")}
-                    />
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
+  Card,
+  CardBody,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+  Row,
+} from "react-bootstrap";
+
+import styles from "./Codigo.module.css";
+import Button from "../../components/Button/button";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+
+function CodigoPage() {
+  const navigate = useNavigate();
+  const inputRefs = useRef([]);
+  const size = 6;
+
+  function checkInput(input) {
+    let letters = [...input];
+    console.log("letters: ", letters);
+    return letters;
   }
-  
-  export default CodigoPage;
-  
+
+  function handleChange(event, id) {
+    const value = event.target.value;
+    const letters = checkInput(value);
+    console.log(letters);
+    if (letters.length > 1) {
+      letters.forEach((letter, index) => {
+        const currentInput = inputRefs.current[id + index];
+        if (currentInput) {
+          currentInput.value = letter;
+        }
+      });
+      const lastFilledInputIndex = id + letters.length - 1;
+      const isLastInput = lastFilledInputIndex >= inputRefs.current.length - 1;
+
+      if (isLastInput) inputRefs.current[inputRefs.current.length - 1].focus();
+      else inputRefs.current[lastFilledInputIndex + 1].focus();
+    } else if (value && id < inputRefs.current.length - 1) {
+      inputRefs.current[id + 1].focus();
+    }
+  }
+
+  function handleKeyDown(event, id) {
+    if (event.key === "Backspace") {
+      if (inputRefs.current[id].value === "") {
+        if (id > 0) {
+          inputRefs.current[id - 1].focus();
+        }
+      } else {
+        inputRefs.current[id].value = "";
+      }
+    }
+  }
+
+  return (
+    <>
+      <Container style={{ marginTop: "8em" }}>
+        <Row className={styles.container}>
+          <Col sm={8} lg={6}>
+            <Card>
+              <CardBody>
+                <CardTitle className={styles.title}>
+                  Inserir Código de Verificação
+                </CardTitle>
+                <CardText className={styles.inputs}>
+                  {Array.from({ length: size }).map((_, index) => (
+                    <Col key={index} className={styles.col} xs={1}>
+                      <input
+                        type="number"
+                        id={index}
+                        ref={(el) => (inputRefs.current[index] = el)}
+                        onChange={(e) => handleChange(e, index)}
+                        onKeyDown={(e) => handleKeyDown(e, index)}
+                        maxLength={1}
+                        className={styles.inputCode}
+                      />
+                    </Col>
+                  ))}
+                </CardText>
+                <div className={styles.btnGroup}>
+                  <Button text={"Verificar Código"} />
+                  <Button
+                    text={"Cancelar"}
+                    type={"cancel"}
+                    onClick={() => navigate("/")}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default CodigoPage;

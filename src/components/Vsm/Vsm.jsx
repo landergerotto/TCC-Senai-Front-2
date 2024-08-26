c7344b277ea862f332dc59e3a446ad8fd8f7bd8d
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-08-26 10:18:37 -0300

    i got proxied

--- src/components/Vsm/Vsm.jsx
+++ src/components/Vsm/Vsm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./Vsm.module.css"; // Make sure you have this CSS file
 import { apiUrl } from "../../Api/apiUrl";
+import VsmCard from "../VsmCard/VsmCard";
 
 function Vsm() {
   const [data, setData] = useState([]);
@@ -8,6 +9,7 @@ function Vsm() {
   useEffect(() => {
     apiUrl
       .get("/vsm/get")
+      // .get("/process/get")
       .then((response) => {
         setData(response.data);
         console.log(response.data);
@@ -22,39 +24,13 @@ function Vsm() {
       {data.length === 0 ? (
         <p>Loading data...</p>
       ) : (
-        <ul>
+        <>
           {data.map((item) => (
-            <li key={item.id}>
-              <div>
-                <h3>Batch Info</h3>
-                <p>Batch ID: {item.BatchId}</p>
-                <p>Batch Quantity: {item.BatchQnt}</p>
-                <p>Scrap Quantity: {item.ScrapQnt}</p>
-                <p>Movement: {item.Movement}</p>
-                <p>Interditated: {item.Interditated ? 'Yes' : 'No'}</p>
-                <p>Created At: {new Date(item.created_at).toLocaleString()}</p>
-              </div>
-              <div>
-                <h3>Process Info</h3>
-                <p>Process ID: {item.Process?.id}</p>
-                <p>Process Name: {item.Process?.Name}</p>
-                <p>Type: {item.Process?.Type}</p>
-                <p>OEE: {item.Process?.OEE}</p>
-                <p>Order: {item.Process?.Order}</p>
-                <p>POT: {item.Process?.POT}</p>
-                <p>Created At: {new Date(item.Process?.created_at).toLocaleString()}</p>
-              </div>
-              <div>
-                <h3>User Info</h3>
-                <p>User Name: {item.User?.DisplayName}</p>
-                <p>Email: {item.User?.Email}</p>
-                <p>First Name: {item.User?.FirstName}</p>
-                <p>Last Name: {item.User?.LastName}</p>
-                <p>Created At: {new Date(item.User?.created_at).toLocaleString()}</p>
-              </div>
-            </li>
+            <div key={item.id}>
+              <VsmCard POC={item} Process={item.Process} User={item.User}/>
+            </div>
           ))}
-        </ul>
+        </>
       )}
     </div>
   );
---
+++ src/components/VsmCard/VsmCard.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { apiUrl } from "../../Api/apiUrl";
+import styles from "./VsmCard.module.css"; // Make sure you have this CSS file
+
+function VsmCard({ POC, Process, User }) {
+  return (
+    <>
+      <div className={styles.cardContainer}>
+        <div className={styles.section}>
+          <h3 className={styles.title}>Batch Info</h3>
+          <p>Batch ID: {POC?.BatchId}</p>
+          <p>Batch Quantity: {POC?.BatchQnt}</p>
+          <p>Scrap Quantity: {POC?.ScrapQnt}</p>
+          <p>Movement: {POC?.Movement}</p>
+          <p>Interditated: {POC?.Interditated ? "Yes" : "No"}</p>
+          <p>Created At: {new Date(POC.created_at).toLocaleString()}</p>
+        </div>
+        <div className={styles.section}>
+          <h3 className={styles.title}>Process Info</h3>
+          <p>Process ID: {Process?.id}</p>
+          <p>Process Name: {Process?.Name}</p>
+          <p>Type: {Process?.Type}</p>
+          <p>OEE: {Process?.OEE}</p>
+          <p>Order: {Process?.Order}</p>
+          <p>POT: {Process?.POT}</p>
+          <p>Created At: {new Date(POC.created_at).toLocaleString()}</p>
+        </div>
+        <div className={styles.section}>
+          <h3 className={styles.title}>User Info</h3>
+          <p>User Name: {User?.DisplayName}</p>
+          <p>Email: {User?.Email}</p>
+          <p>First Name: {User?.FirstName}</p>
+          <p>Last Name: {User?.LastName}</p>
+          <p>Created At: {new Date(POC.created_at).toLocaleString()}</p>
+        </div>
+        <div className={styles.footer}>Shared</div>
+      </div>
+    </>
+  );
+}
+
+export default VsmCard;

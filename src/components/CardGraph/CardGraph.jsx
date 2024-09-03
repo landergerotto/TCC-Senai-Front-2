980b741dd93aad26ff2c7fb20dcc159a0e8fc0a9
eric-coutinho <eric.coutinhop@hotmail.com>
2024-09-03 11:05:51 -0300

    working on cards on dash page

---
+++ src/components/CardGraph/CardGraph.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-dupe-keys */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import styles from "./CardGraph.module.css";
+
+function CardGraph({ data }) {
+  const [bgColor, setBgColor] = useState("");
+  console.log("data: ", data);
+
+  useEffect(() => {
+    if (data.value == 1)
+      setBgColor("linear-gradient(to right, #FF4B2B, #FF416C)"); // red
+    else if (data.value == 2) {
+      setBgColor("linear-gradient(to left, #2c3e50, #bdc3c7)"); // gray
+    } else {
+      setBgColor("linear-gradient(20deg, #0ba360 0%, #3cba92 100%)"); // green
+    }
+  }, []);
+
+  return (
+    <div className={styles.cardBg} style={{ background: bgColor }}>
+      {data.label}:
+      <div>{data.value}</div>
+    </div>
+  );
+}
+
+export default CardGraph;
--- src/pages/Relatorios/Relatorios.jsx
+++ src/pages/Relatorios/Relatorios.jsx
@@ -1,13 +1,58 @@
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+import { Col, Container, Row } from "react-bootstrap";
+
+import CardGraph from "../../components/CardGraph/CardGraph";
+
+import { apiUrl } from "../../Api/apiUrl";
 
 function RelatoriosPage() {
-    return (
-        <>
-            <Container>
-                aaa
-            </Container>
-        </>
-    )
+  const [data, setData] = useState({});
+
+  const card1Data = {
+    label: "Total de Peças",
+    value: "100",
+  };
+
+  const card2Data = {
+    label: "Peças em Refugo",
+    value: "2",
+  };
+
+  const card3Data = {
+    label: "Tempo Total Parado",
+    value: "1",
+  };
+
+  useEffect(() => {
+    apiUrl
+      .get("poc/get")
+      .then((response) => {
+        console.log("response: ", response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log("Erro ao buscar dados do processo: ", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <CardGraph data={card1Data} />
+          </Col>
+          <Col>
+            <CardGraph data={card2Data} />
+          </Col>
+          <Col>
+            <CardGraph data={card3Data} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default RelatoriosPage;
